f58b67e061b75f8eb86ba4358af0d1864c18b150
Xx0x16z <Xx0x16z@outlook.com>
2026-06-28 00:47:24 +0800

    MORE STYL:ES!
    who ask you?

--- p3n1sFunctionCreator.py
+++ p3n1sFunctionCreator.py
@@ -5,43 +5,74 @@ PEN_IS_LENGTH = 0
 TRAJECTORY = False
 TRAJECTORY_MAX_Y = 0
 TRAJECTORY_MAX_SIZE = 0
+left_circle_center_x = 0
+right_circle_center_x = 0
+circle_top_y = 0
+top_circle_center_y = 0
+square_of_size = 0
+BALL_STYLE = 0
+TRAJECTORY_STYLE = 0
 
 
-def replaceText(text):
-    return text.replace("-0.0 ", "").replace("+0.0 ", "").replace("--", "+").replace(" )", ")").replace("(x)",
-                                                                                                        "x").replace(
-        "(y)", "y")
+def simplifier(text):
+    return text.replace("-0.0 ", "").replace("+0.0 ", "").replace("--", "+").replace("(x)", "x").replace("(y)", "y").replace("+-", "-")
 
 
-def final_result_0():
-    return replaceText(f"""
-            1.
-            (x-{CENTER_POSITION_X - BALL_SIZE} )^2+(y-{CENTER_POSITION_Y} )^2={BALL_SIZE * BALL_SIZE}
-            2.
-            (x-{CENTER_POSITION_X + BALL_SIZE} )^2+(y-{CENTER_POSITION_Y} )^2={BALL_SIZE * BALL_SIZE}
-            3.
-            x={CENTER_POSITION_X - BALL_SIZE}+0*sqrt((y-{CENTER_POSITION_Y + BALL_SIZE} )*({CENTER_POSITION_Y + BALL_SIZE + PEN_IS_LENGTH}-y ))
-            4.
-            x={CENTER_POSITION_X + BALL_SIZE}+0*sqrt((y-{CENTER_POSITION_Y + BALL_SIZE} )*({CENTER_POSITION_Y + BALL_SIZE + PEN_IS_LENGTH}-y ))
-            5.
-            0*sqrt(y-{CENTER_POSITION_Y + BALL_SIZE + PEN_IS_LENGTH} )+(x-{CENTER_POSITION_X} )^2+(y-{CENTER_POSITION_Y + BALL_SIZE + PEN_IS_LENGTH} )^2={BALL_SIZE * BALL_SIZE}
-            """)
+def ballStyleFullCircle():
+    return f"""[].
+            (x-{left_circle_center_x})^2+(y-{CENTER_POSITION_Y})^2={square_of_size}
+            [].
+            (x-{right_circle_center_x})^2+(y-{CENTER_POSITION_Y})^2={square_of_size}
+            """
 
 
-def final_result_1():
-    return replaceText(f"""6.
-            {CENTER_POSITION_Y + PEN_IS_LENGTH + BALL_SIZE * 2 + TRAJECTORY_MAX_Y}>y>{TRAJECTORY_MAX_SIZE}x^2-{2 * TRAJECTORY_MAX_SIZE * CENTER_POSITION_X}x+{CENTER_POSITION_Y + PEN_IS_LENGTH + BALL_SIZE * 2 + TRAJECTORY_MAX_SIZE * CENTER_POSITION_X * CENTER_POSITION_X}
-            """)
+def ballStyleHalfCircle():
+    return f"""[].
+            (x-{left_circle_center_x})^2+(y-{CENTER_POSITION_Y})^2={square_of_size}+0*sqrt(-x+{left_circle_center_x})
+            [].
+            (x-{right_circle_center_x})^2+(y-{CENTER_POSITION_Y})^2={square_of_size}+0*sqrt(x-{right_circle_center_x})
+            """
+
+
+def main_part():
+    return f"""[].
+            x={left_circle_center_x}+0*sqrt(y-{circle_top_y})*sqrt({top_circle_center_y}-y)
+            [].
+            x={right_circle_center_x}+0*sqrt(y-{circle_top_y})*sqrt({top_circle_center_y}-y)
+            [].
+            0*sqrt(y-{top_circle_center_y})+(x-{CENTER_POSITION_X})^2+(y-{top_circle_center_y})^2={square_of_size}
+            """
+
+
+def trajectoryStyleQuadratic():
+    return f"""[].
+            {top_circle_center_y + BALL_SIZE + TRAJECTORY_MAX_Y}>y>{TRAJECTORY_MAX_SIZE}x^2-{2 * TRAJECTORY_MAX_SIZE * CENTER_POSITION_X}x+{top_circle_center_y + BALL_SIZE + TRAJECTORY_MAX_SIZE * CENTER_POSITION_X * CENTER_POSITION_X}
+            """
+
+
+def trajectoryStyleLine():
+    return f"""[].
+            {top_circle_center_y + BALL_SIZE + TRAJECTORY_MAX_Y}>y>{TRAJECTORY_MAX_SIZE}|x-{CENTER_POSITION_X}|+{top_circle_center_y + PEN_IS_LENGTH + BALL_SIZE + TRAJECTORY_MAX_Y - TRAJECTORY_MAX_SIZE * CENTER_POSITION_X}
+            """
 
 
 if __name__ == '__main__':
     CENTER_POSITION_X = float(input("center x:"))
     CENTER_POSITION_Y = float(input("center y:"))
+    BALL_STYLE = int(input("ball style(0->full,1->half):"))
     BALL_SIZE = float(input("size:"))
     PEN_IS_LENGTH = float(input("length:"))
 
     TRAJECTORY = bool(float(input("with trajectory?(1->yes,0->no):")))
-    TRAJECTORY_MAX_Y = float(input("trajectory max y:"))
-    TRAJECTORY_MAX_SIZE = abs(float(input("trajectory max size:")))
+    if TRAJECTORY:
+        TRAJECTORY_STYLE = int(input("trajectory style(0->quadratic,1->line):"))
+        TRAJECTORY_MAX_Y = float(input("trajectory max y:"))
+        TRAJECTORY_MAX_SIZE = abs(float(input("trajectory max size:")))
 
-    print(final_result_0() + ("" if not TRAJECTORY else final_result_1()))
+    left_circle_center_x = CENTER_POSITION_X - BALL_SIZE
+    right_circle_center_x = CENTER_POSITION_X + BALL_SIZE
+    circle_top_y = CENTER_POSITION_Y + BALL_SIZE
+    top_circle_center_y = CENTER_POSITION_Y + BALL_SIZE + PEN_IS_LENGTH
+    square_of_size = BALL_SIZE * BALL_SIZE
+
+    print(simplifier((ballStyleFullCircle() if BALL_STYLE == 0 else ballStyleHalfCircle()) + main_part() + (trajectoryStyleQuadratic() if TRAJECTORY_STYLE == 0 else trajectoryStyleLine())))
